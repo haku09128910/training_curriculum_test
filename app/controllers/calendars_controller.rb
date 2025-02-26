f5924ca5a3a8be3b95198e5fed5b7d0bd0e734ec
haku09128910 <h0912-8910@au.com>
2025-02-26 22:15:18 +0900

    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    # binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -20,24 +19,29 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-    # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
+    week_days = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+  
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
     @week_days = []
-
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+  
+    plans = Plan.where(date: @todays_date..@todays_date + 7)
+  
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+  
+      # 曜日を取得し、daysハッシュに追加
+      wday = (@todays_date + x).wday # wdayメソッドで曜日（0=日曜, 1=月曜, ..., 6=土曜）を取得
+      days = { 
+        month: (@todays_date + x).month, 
+        date: (@todays_date + x).day, 
+        wday: week_days[wday], # 曜日を追加
+        plans: today_plans
+      }
+  
       @week_days.push(days)
     end
-
   end
 end
